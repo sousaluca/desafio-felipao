ee21989420e3cb7eff287814cffbaf8603e0d45f
sousaluca <oliveiraknoa@gmail.com>
2025-08-22 16:02:49 -0300

    projeto criado

---
+++ index.js
@@ -0,0 +1,32 @@
+const prompt = require("prompt-sync")();
+
+while (true) {
+    let nome = prompt("Digite o nome do herói: ");
+    let xp = parseInt(prompt("Digite a quantidade de XP: "));
+
+    let nivel = "";
+
+    if (xp < 1000) {
+        nivel = "Ferro";
+    } else if (xp >= 1001 && xp <= 2000) {
+        nivel = "Bronze";
+    } else if (xp >= 2001 && xp <= 5000) {
+        nivel = "Prata";
+    } else if (xp >= 5001 && xp <= 7000) {
+        nivel = "Ouro";
+    } else if (xp >= 7001 && xp <= 8000) { 
+        nivel = "Platina";
+    } else if (xp >= 8001 && xp <= 9000) {  
+        nivel = "Ascendente";
+    } else if (xp >= 9001 && xp <= 10000) {
+        nivel = "Imortal";
+    } else {
+        nivel = "Radiante";
+    }
+    console.log(`O herói ${nome} está no nível ${nivel}.`);
+
+    let continuar = prompt("Deseja continuar? (s/n): ");
+    if (continuar.toLowerCase() !== 's') {  
+        console.log("Programa encerrado.");
+        break;
+    }
